e76cbd4d1f103d93efee6f34d340254b8a43be95
Kevin Tran <ktran@bashx3.ca>
2019-11-07 14:48:59 -0500

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,3 @@
-// const mysql = require('mysql')
-// const pool  = mysql.createPool({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'root',
-//     password: '14151415',
-//     database: 'techlink-1'
-// })
-
 import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(
