429fa6bc4f2166d9070ad43df2f2e8fc0f9a5608
5PK <me@ktran.info>
2019-11-07 15:43:39 -0500

    Add configuration for Heroku deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,21 @@
 import Sequelize from 'sequelize';
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
 const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD,
-    {
-      dialect: 'mysql',
-    },
+  isProduction ? process.env.DATABASE_URL : connectionString,
+  {
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+  
   );
 
 module.exports = sequelize
+
